f4124223b20fe447cf545f020a9c11b6dce45569
FriChaFri <gitfrichacha@protonmail.com>
2025-05-13 14:30:28 -0600

    added constants, cahnged data

---
+++ src/constants.py
@@ -0,0 +1,11 @@
+# Hyperparameters
+BATCH_SIZE = 64
+LEARNING_RATE = 3e-4
+EPOCHS = 10
+
+# Data paths
+DATA_DIR = "data/MNIST"
+MODEL_SAVE_PATH = "models/cnn_model.pkl"
+
+# Random seed
+SEED = 5678
--- src/data.py
+++ src/data.py
@@ -2,22 +2,50 @@
 
 import torch
 import torchvision
+from constants import BATCH_SIZE
 
-normalise_data = torchvision.transforms.Compose(
-    [
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize((0.5,), (0.5,)),
-    ]
-)
-train_dataset = torchvision.datasets.MNIST(
-    root="data/MNIST",
-    train=True,
-    download=True,
-    transform=normalise_data,
-)
-test_dataset = torchvision.datasets.MNIST(
-    root="data/MNIST",
-    train=False,
-    download=True,
-    transform=normalise_data,
-)
+def load_data(BATCH_SIZE=BATCH_SIZE):
+    normalise_data = torchvision.transforms.Compose(
+        [
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.5,), (0.5,)),
+        ]
+    )
+    train_dataset = torchvision.datasets.MNIST(
+        root="data/MNIST",
+        train=True,
+        download=True,
+        transform=normalise_data,
+    )
+    test_dataset = torchvision.datasets.MNIST(
+        root="data/MNIST",
+        train=False,
+        download=True,
+        transform=normalise_data,
+    )
+    trainloader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=BATCH_SIZE, shuffle=True
+    )
+    testloader = torch.utils.data.DataLoader(
+        test_dataset, batch_size=BATCH_SIZE, shuffle=True
+    )
+    return trainloader, testloader
+
+
+def show_batch(loader):
+    import matplotlib.pyplot as plt
+    images, labels = next(iter(loader))
+    images = images.numpy()
+    
+    fig, axs = plt.subplots(1, 8, figsize=(12, 2))
+    for i in range(8):
+        axs[i].imshow(images[i][0], cmap="gray")
+        axs[i].set_title(f"Label: {labels[i].item()}")
+        axs[i].axis('off')
+    plt.savefig("fig/sample_input_batch.png")
+
+
+if __name__ == "__main__":
+    trainloader, testloader = load_data()
+    print("Loaded data successfully")
+    show_batch(trainloader)
